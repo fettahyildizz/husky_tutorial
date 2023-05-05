15cd128b875306075fb198ab930751a84cbb5d5a
fettahyildiz <fettah.yildiz@delivers.ai>
2023-05-05 17:47:13 +0300

    init

---
+++ husky/husky_navigation/src/local_planner.cpp
@@ -0,0 +1,51 @@
+#include "husky_navigation/local_planner.h"
+#include "geometry_msgs/PoseStamped.h"
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(local_planner::LocalPlanner, nav_core::BaseLocalPlanner)
+
+namespace local_planner {
+LocalPlanner::LocalPlanner()
+    : costmap_ros_(NULL), tf_(NULL), initialized_(false) {}
+LocalPlanner::LocalPlanner(std::string name, tf2_ros::Buffer *tf,
+                           costmap_2d::Costmap2DROS *costmap_ros)
+    : costmap_ros_(NULL), tf_(NULL), initialized_(false) {
+  initialize(name, tf, costmap_ros);
+}
+
+LocalPlanner::~LocalPlanner() {}
+
+void LocalPlanner::initialize(std::string name, tf2_ros::Buffer *tf,
+                              costmap_2d::Costmap2DROS *costmap_ros) {
+  if (!initialized_) {
+    tf_ = tf;
+    costmap_ros_ = costmap_ros;
+    initialized_ = true;
+  }
+}
+
+bool LocalPlanner::setPlan(
+    const std::vector<geometry_msgs::PoseStamped> &orig_global_plan) {
+  if (!initialized_) {
+    ROS_ERROR("This planner has not been initialized");
+    return false;
+  }
+  return true;
+}
+
+bool LocalPlanner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel) {
+  if (!initialized_) {
+    ROS_ERROR("This planner has not been initialized");
+    return false;
+  }
+  return true;
+}
+
+bool LocalPlanner::isGoalReached() {
+  if (!initialized_) {
+    ROS_ERROR("This planner has not been initialized");
+    return false;
+  }
+  return false;
+}
+}; // namespace local_planner
